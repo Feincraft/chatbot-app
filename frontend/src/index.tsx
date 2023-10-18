676d6cd91b152d422e6981d31c7bca5099c77b70
Ilya <32096531+Feincraft@users.noreply.github.com>
2023-10-18 21:02:17 +0200

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { initializeIcons } from "@fluentui/react";
 
-import "./index.css";
+//import "./index.css";
 
 import Layout from "./pages/layout/Layout";
 import NoPage from "./pages/NoPage";
